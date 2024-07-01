1b03411c415faabd10e84585a5541d70e5994289
Yoda-Flash <kaitlyn.zee@gmail.com>
2024-07-01 09:58:41 +0800

    Working get plain message and get key functions, move on to the coded message next

--- encrypt.go
+++ encrypt.go
@@ -1,21 +1,53 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"os"
 )
 
-func getPlainMessage() {
+func getPlainMessage(scanner *bufio.Scanner) string {
 	var plainMessage string
 	fmt.Println("Please input your message:")
-	count, err := fmt.Scanln(&plainMessage)
-	//plainMessage, err := fmt.Scanln(&plainMessage)
+	scanner.Scan()
+	err := scanner.Err()
+	plainMessage = scanner.Text()
+	//count, err := fmt.Scanln(&plainMessage)
+	////plainMessage, err := fmt.Scanln(&plainMessage)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(plainMessage)
+	//fmt.Println(plainMessage)
+	//fmt.Println(count)
+	//for i := 0; i < count; i++ {
+	//	fmt.Println(plainMessage)
+	//}
+	return plainMessage
+}
+
+func getKey(scanner *bufio.Scanner) string {
+	fmt.Println("Please input your key, without any spaces in between:")
+	scanner.Scan()
+	err := scanner.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var key string
+	key = scanner.Text()
+	fmt.Println(key)
+	return key
+}
+
+func getCodedMessage(plainMessage string, key string) string {
+
 }
 
 func main() {
-	getPlainMessage()
+	scanner := bufio.NewScanner(os.Stdin)
+	plainMessage := getPlainMessage(scanner)
+	key := getKey(scanner)
+	codedMessage := getCodedMessage(plainMessage, key)
+	fmt.Println(codedMessage)
 }
